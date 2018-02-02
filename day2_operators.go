11da6cf8c47137b0cf7076103637561823282fed
ykaseng <kaseng.y@gmail.com>
2018-02-02 16:56:33 +0800

    Complete Day 3

--- day2_operators.go
+++ day2_operators.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func main() {
+func operators() {
 	var (
 		mealCost   float64
 		tipPercent int
@@ -16,7 +16,7 @@ func main() {
 	fmt.Scanf("%d\n", &tipPercent)
 	fmt.Scanf("%d\n", &taxPercent)
 
-	round(mealCost + (float64(tipPercent) / 100 * mealCost) + (float64(taxPercent) / 100 * mealCost))
+	fmt.Printf("The total meal cost is %v dollars.", round(mealCost+(float64(tipPercent)/100*mealCost)+(float64(taxPercent)/100*mealCost)))
 }
 
 func round(f float64) float64 {
---
+++ day3_conditional_statements.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d\n", &n)
+
+	if n%2 != 0 {
+		fmt.Println("Weird")
+	} else {
+		if 2 < n && n < 5 {
+			fmt.Println("Not Weird")
+		} else if 6 < n && n < 20 {
+			fmt.Println("Weird")
+		} else if n > 20 {
+			fmt.Println("Not Weird")
+		}
+	}
+
+}
